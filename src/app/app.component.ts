94e63f3535b22455d76c5a0788115b3294d1fa9d
ShoneUnity <e10789akansh@dpsrkp.net>
2023-03-05 11:45:34 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'password';
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+  length = 0;
+  password = '';
+
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+  onButtonClicked() {
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const symbols = '!@#$%^&*()';
+
+    let validCharacters = '';
+    if (this.includeLetters) {
+      validCharacters += letters;
+    }
+    if (this.includeNumbers) {
+      validCharacters += numbers;
+    }
+    if (this.includeSymbols) {
+      validCharacters += symbols;
+    }
+
+    let generatedPassword = '';
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validCharacters.length);
+      generatedPassword += validCharacters[index];
+    }
+    this.password = generatedPassword;
+  }
+
+  onChangeLength(event: Event) {
+    this.length = parseInt((event.target as HTMLInputElement).value);
+  }
 }
